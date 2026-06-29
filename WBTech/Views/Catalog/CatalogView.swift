5ceeceb8dc067bd333d619c1ac58a63d5beae674
sye7qjm3ac <sye7qjm3ac@icloud.com>
2026-06-29 21:52:53 +0700

    feat: finish catalog UI

---
+++ WBTech/Views/Catalog/CatalogView.swift
@@ -0,0 +1,43 @@
+//
+
+import OSLog
+import SwiftUI
+
+struct CatalogView: View {
+  let catalogService: CatalogServiceProtocol
+  
+  @State private var categories: [Category] = []
+  @State private var isLoading = true
+  
+  var body: some View {
+    NavigationView {
+      ScrollView {
+        switch isLoading {
+        case true:
+          ProgressView()
+        case false:
+          CatalogContentView(categories: categories)
+        }
+      }
+      .task {
+        await loadData()
+      }
+    }
+  }
+  
+  private func loadData() async {
+    isLoading = true
+    
+    do {
+      categories = try await catalogService.fetchCategories()
+    } catch {
+      Logger.catalog.error("Ошибка загрузки категорий: \(error.localizedDescription)")
+    }
+    
+    isLoading = false
+  }
+}
+
+#Preview {
+  CatalogView(catalogService: MockCatalogService())
+}
