a06ae8cd7b21a43f24d85b7822ab9ac68f5774b4
sye7qjm3ac <sye7qjm3ac@icloud.com>
2026-06-29 20:05:32 +0700

    feat: extend Logger

---
+++ WBTech/Extensions/Logger+App.swift
@@ -0,0 +1,10 @@
+//
+
+import Foundation
+import OSLog
+
+extension Logger {
+  private static let subsystem = Bundle.main.bundleIdentifier ?? "ru.t02.WBTech"
+  
+  static let catalog = Logger(subsystem: subsystem, category: "Catalog")
+}
