6b2990300d25bb7cbeb1c65cd5146a943d9bef32
sye7qjm3ac <sye7qjm3ac@icloud.com>
2026-07-06 17:38:12 +0700

    feat: add favorites store

--- WBTech/Extensions/Logger+App.swift
+++ WBTech/Extensions/Logger+App.swift
@@ -8,5 +8,7 @@ extension Logger {
   private static let subsystem = Bundle.main.bundleIdentifier ?? "ru.t02.WBTech"
   
   static let catalog = Logger(subsystem: subsystem, category: "Catalog")
+  static let cart = Logger(subsystem: subsystem, category: "Cart")
+  static let favorites = Logger(subsystem: subsystem, category: "Favorite")
   
 }
