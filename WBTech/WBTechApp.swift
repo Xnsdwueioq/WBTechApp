89c41f2380ce5cd5e60bb44f4c0d0918bf0f41c7
sye7qjm3ac <sye7qjm3ac@icloud.com>
2026-07-08 19:16:46 +0700

    feat: add ui test for the catalog flow

--- WBTech/WBTechApp.swift
+++ WBTech/WBTechApp.swift
@@ -9,11 +9,19 @@ struct WBTechApp: App {
   let favoritesService: FavoritesServiceProtocol
   
   init() {
-    TokenBootstrap.run()
-    let token = KeychainStore.read(account: TokenBootstrap.account) ?? ""
-    self.catalogService = CatalogService(token: token)
-    self.cartService = CartService(token: token)
-    self.favoritesService = FavoritesService(token: token)
+    let isUITesting = ProcessInfo.processInfo.arguments.contains("UITESTS")
+    
+    if isUITesting {
+      self.catalogService = MockCatalogService()
+      self.cartService = MockCartService()
+      self.favoritesService = MockFavoritesService()
+    } else {
+      TokenBootstrap.run()
+      let token = KeychainStore.read(account: TokenBootstrap.account) ?? ""
+      self.catalogService = CatalogService(token: token)
+      self.cartService = CartService(token: token)
+      self.favoritesService = FavoritesService(token: token)
+    }
   }
   
   var body: some Scene {
