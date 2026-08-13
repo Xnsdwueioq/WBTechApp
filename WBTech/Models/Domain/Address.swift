c6fcdba04bf5ded00140828ad1129b31a4b6159b
sye7qjm3ac <sye7qjm3ac@icloud.com>
2026-08-13 20:31:31 +0700

    fix(address): add coordinates and comment to the address domain model

--- WBTech/Models/Domain/Address.swift
+++ WBTech/Models/Domain/Address.swift
@@ -1,9 +1,16 @@
 //
 
+struct AddressCoordinates: Hashable, Sendable {
+  let longitude: Double
+  let latitude: Double
+}
+
 struct Address: Identifiable, Hashable, Sendable {
   let id: String
+  let coordinates: AddressCoordinates
   let addressLine: String
   let floor: String?
   let entrance: String?
   let intercomCode: String?
+  let comment: String?
 }
--- WBTech/Services/Order/MockOrderService.swift
+++ WBTech/Services/Order/MockOrderService.swift
@@ -10,10 +10,12 @@ struct MockOrderService: OrderServiceProtocol {
     return [
       Address(
         id: "address1",
+        coordinates: .init(longitude: 1.2, latitude: 0.9),
         addressLine: "Новая Басманная ул., 35 ст1, 59",
         floor: "3",
         entrance: "4",
-        intercomCode: "15809"
+        intercomCode: "15809",
+        comment: "Какой-то комментарий"
       )
     ]
   }
