ae26707b0cd84388743ef1b7748fdad6129fcc34
sye7qjm3ac <sye7qjm3ac@icloud.com>
2026-08-18 20:13:18 +0700

    feat(address): add map with buttons without actions

--- UISystem/Sources/UISystem/Styles/ButtonStyles/DSMapControlButtonStyle.swift
+++ UISystem/Sources/UISystem/Styles/ButtonStyles/DSMapControlButtonStyle.swift
@@ -8,33 +8,10 @@
 import SwiftUI
 
 public struct DSMapControlButtonStyle: ButtonStyle {
-  private enum Layout {
-    static let cornerRadius: CGFloat = 12
-    static let shadowRadius: CGFloat = 10
-    static let shadowOffsetY: CGFloat = -1
-  }
-
   public init() {}
 
   public func makeBody(configuration: Configuration) -> some View {
     configuration.label
-      .background {
-        RoundedRectangle(
-          cornerRadius: Layout.cornerRadius,
-          style: .continuous
-        )
-        .fill(Color.dsMapControlBackground)
-        .shadow(
-          color: Color.dsMapControlShadow,
-          radius: Layout.shadowRadius,
-          y: Layout.shadowOffsetY
-        )
-      }
-      .contentShape(
-        RoundedRectangle(
-          cornerRadius: Layout.cornerRadius,
-          style: .continuous
-        )
-      )
+      .modifier(DSMapControlBackgroundViewModifier())
   }
 }
